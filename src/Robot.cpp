a42330b69cc8dac59811e844d4bc698de0fa57cc
Paul Ugolini <ugolinipaul@gmail.com>
2015-03-06 00:20:19 -0500

    added auto+tilt, disabled segfaulting i2c code

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,6 +1,11 @@
-// CB3 Ver .75
+// CB3 Ver .81
 //
 //     Change log
+//  1 Mar Verified BuiltInAccel working, installed gyro too (mechanum!)
+//          finished first version of auton.
+// 28 Feb reset for Arduino mapped to gamepad "start" button
+//          a first touch of Auton code. Fixed BuiltinAccelerometer calls
+// 26 Feb basic arm and gantry limit switch stops implemented
 // 16 Feb wiremap at bottom, I2C sections vestigial (not workin')
 // 16 Feb mapped claw to a spare victor/PWM pair.
 // 14 Feb added accelerometer tilt output
@@ -20,19 +25,26 @@
 //#define MECHANUM
 #define TANK
 
+
+
+
 /*******************************/
 /**********BUILD FLAGS**********/
 /*******************************/
 
 #define CLRFAULTS
 
-
-
-float rx,ry,rax,ray,lay,lax;
+float rx,ry,rax,ray,lay,lax, time_auton, pi;
 float tilt_theta, tilt_phi, gmag, gmin, gmax, Bx, By, Bz, Bmag, Bmin, Bmax, heading;
-int counti2c = 0;
+int counti2c, switches, autoPeriodicLoops, liftPosn, rightArmPosn, leftArmPosn;
+int leftDrivePosn, rightDrivePosn;
+// all Auton Behavior state specs
+int firstDrive, secondDrive, RarmPreset,LarmPreset,speed_x,speed_y, backup;
+int delay, lift_this_high, drop_this_low,  behavior_auton, completel, completer;
 uint8_t data;
 long EncoderOffset1,EncoderOffset2,EncoderOffset3;
+// all of these booleans are images of the limit microswitches. '1' is switch closed
+bool lifttop, liftbottom, LarmL, LarmR, RarmL, RarmR, clawbottom, driveDone;
 //int lastLiftCount,currentLiftCount;
 //computed from 45*M_PI/180
 #define sncs 0.707106781187;
@@ -40,20 +52,20 @@ long EncoderOffset1,EncoderOffset2,EncoderOffset3;
 
 class Robot: public IterativeRobot
 {
-
-
 	Joystick gamePad,leftStick,rightStick;
 	LiveWindow *lw;
 	int autoLoopCounter;
 	PowerDistributionPanel PDB;
 	ITable *hi;
 	ITable *table;
-	//Gyro rateGyro;
+	Gyro rateGyro;
 	Talon fRight,fLeft,bRight,bLeft;
 	VictorSP gearMotor,rightArm,leftArm, claw;
 	RobotDrive robotDrive;
 	Encoder liftPos,leftArmEncoder,rightArmEncoder,lDriveEncoder,rDriveEncoder;
 	Accelerometer *accel;
+	DigitalOutput arduinoReset;
+	Timer timed;
 
 public:
 	Robot() :
@@ -65,7 +77,7 @@ public:
 		PDB(),
 		hi(),
 		table(),
-		//rateGyro(0),
+		rateGyro(0),
 		fRight(0),
 		fLeft(2),
 		bRight(1),
@@ -79,8 +91,9 @@ public:
 		leftArmEncoder(6,7),
 		rightArmEncoder(4,5),
 		lDriveEncoder(2,3),
-		rDriveEncoder(0,1)
-	{
+		rDriveEncoder(0,1),
+		arduinoReset(14)
+{
 		PDB.InitTable(hi);
 		PDB.StartLiveWindowMode();
 		liftPos.InitTable(table);
@@ -93,28 +106,31 @@ public:
 		lDriveEncoder.StartLiveWindowMode();
 		rDriveEncoder.InitTable(table);
 		rDriveEncoder.StartLiveWindowMode();
-	}
+		//		accel.InitTable(table);
+}
 
 private:
 
 	void RobotInit()
 	{
 		lw = LiveWindow::GetInstance();
-		#ifdef CLRFAULTS
-			PDB.ClearStickyFaults();
-		#endif
+#ifdef CLRFAULTS
+		PDB.ClearStickyFaults();
+#endif
+
 		liftPos.Reset();
 		rightArmEncoder.Reset();
 		leftArmEncoder.Reset();
 		lDriveEncoder.Reset();
 		rDriveEncoder.Reset();
-
-		accel = new BuiltInAccelerometer(Accelerometer::Range::kRange_2G);
-	             // setup values
-	gmax=12.0;   //  in m/s^2
-	gmin=8.0;
-	Bmax=680.0;  //  in milligauss
-	Bmin=410.0;
+		timed.Reset();
+		accel = new BuiltInAccelerometer(Accelerometer::Range::kRange_4G);
+		// setup values
+		gmax=12.0;   //  in m/s^2
+		gmin=8.0;
+		Bmax=680.0;  //  in milligauss
+		Bmin=410.0;
+		pi = 4.0*atan(1.0);
 	}
 	void RobotPeriodic()
 	{
@@ -124,18 +140,136 @@ private:
 		leftArmEncoder.UpdateTable();
 		lDriveEncoder.UpdateTable();
 		rDriveEncoder.UpdateTable();
+		/*
+		// TEST LINES FOR ACCELEROMETER : can remove these in final code.
+			// determine tilt
+						double xVal = accel->GetX();
+						double yVal = accel->GetY();
+						double zVal = accel->GetZ();
+						gmag = xVal*xVal+yVal*yVal+zVal*zVal;
+						if ((gmag<gmax*gmax)&&(gmag>gmin*gmin)) { //validate
+								gmag = pow(gmag,0.5);
+								tilt_theta = acos(zVal/gmag);
+								tilt_phi   = atan(yVal/xVal);
+						}
+						tilt_theta = acos(zVal/gmag);
+						tilt_phi   = atan(yVal/xVal);
+						printf(" tilt theta %f3.1 and  phi %f3.3\n", 180.0*tilt_theta/pi, 180.0*tilt_phi/pi);
+						// end get tilt, REMOVE WHEN DONE TESTING!
+		 *
+		 */
 	}
 
 	void AutonomousInit()
 	{
-
+		autoPeriodicLoops = 0;
+		liftPos.Reset();
+		rightArmEncoder.Reset();
+		leftArmEncoder.Reset();
+		lDriveEncoder.Reset();
+		rDriveEncoder.Reset();
+		timed.Reset();
+		//drive spec init
+		//     Here driving forward for 'firstDrive' then picking up tote and driving for secondDrive' '
+		firstDrive=48;
+		behavior_auton=1; //lsb=drive1, bit2=pickup1, bit3=drive2, bit4=lower package, bit5=drive3
+		speed_x = 50; //drive speed each side, for TANK DRIVE
+		speed_y = 50;
+		delay = 2;             //time delay spec
+		completel = 0;         // flags for completion of arm motions to presets below.
+		completer = 0;
+		RarmPreset = 1200;     // right and left arm preset targets for first tote
+		LarmPreset = 1350;
+		lift_this_high = 1870; //  lift it run for secondDrive
+		drop_this_low = 12;    //  height at which to release the tote
+		secondDrive = 80;      // complete drive and lower and release tote
+		backup = 13;           // then backup away from tote. any subsequent motion?
+		// end of drive spec init
 	}
+	/*
+	void AutonomousPeriodic()
+	{
+		time_auton=timed.Get();
+		// get DOF state
+		liftPosn      = liftPos.GetDistance();
+		rightArmPosn  = rightArmEncoder.GetRaw();
+		leftArmPosn   = leftArmEncoder.GetRaw();
+		leftDrivePosn = lDriveEncoder.GetRaw();
+		rightDrivePosn= rDriveEncoder.GetRaw();
+		//section to determine ordering of events
+		if((rightDrivePosn > firstDrive) && (leftDrivePosn>firstDrive)){
+			behavior_auton=3; // signals move onto pickup
+		}
+		// behavior_auton set to 7 when ready to "drive2" in auton below
+		if((rightDrivePosn > secondDrive) && (leftDrivePosn>secondDrive)){
+			behavior_auton=15; // signals to lower
+		    completel=0;       // flags for the arms to complete open.
+		    completer=0;
+		}
+		// behavior_auton set to 31 when ready to "drive3" drive away
+		if((behavior_auton==15)&&(completel&&completer)){
+			behavior_auton=31;  // ready to back up and/or drive away
+		}
+		if((behavior_auton==31)&&(rightDrivePosn < secondDrive-backup) && (leftDrivePosn>secondDrive-backup)){
+			behavior_auton=65; //end..the flag for no auton action.
+		}
+		Auton(); //Auton function (at end of file)
+		autoPeriodicLoops++;
+	}
+	 */
+
+/*******************************/
+/*************AUTON*************/
+/*******************************/
+
+/*I commented out your code because I didn't have time to go through it and just wanted something quick.
+ * Also, I would like to make presets for the arms, as soon as the encoders are mounted.
+ * */
+
+//Since the drive encoders are 360 ppr, 1 pulse = 1 degree
+//I have no idea what this should be...
+#define AUTON_POS_DIF_THRESHOLD 5000
+#define AUTON_POS_SLOWDOWN_THRESHOLD 500
+#define AUTON_POS_STOP_THRESHOLD 50
+//76 inches: 19 in/rev,360 pulses/rev
+//the 4 is arbitrary
+#define AUTON_POS_TARGET 4*360
+#define AUTON_LEFT_DRIVE_FAST .5
+#define AUTON_RIGHT_DRIVE_FAST .5
+#define AUTON_LEFT_DRIVE_SLOW .1
+#define AUTON_RIGHT_DRIVE_SLOW .1
 
 	void AutonomousPeriodic()
 	{
+		int leftpos = lDriveEncoder.GetRaw() ;
+		int rightpos= rDriveEncoder.GetRaw() ;
+		int diff = abs(leftpos - rightpos);
+		int avgPos = (leftpos + leftpos)/2;
+		if(diff < AUTON_POS_DIF_THRESHOLD)//encoders are close-ish
+		{
+			if(abs(avgPos - AUTON_POS_TARGET) >= AUTON_POS_SLOWDOWN_THRESHOLD)//pos is less than target, by a lot
+			{
+				robotDrive.TankDrive(AUTON_LEFT_DRIVE_FAST,AUTON_RIGHT_DRIVE_FAST);
+			}
+			else if((abs(avgPos - AUTON_POS_TARGET) < AUTON_POS_SLOWDOWN_THRESHOLD) && (abs(avgPos - AUTON_POS_TARGET) >= AUTON_POS_STOP_THRESHOLD))//pos is less than target, by a little
+			{
+				robotDrive.TankDrive(AUTON_LEFT_DRIVE_SLOW,AUTON_RIGHT_DRIVE_SLOW);
+			}
+			else if(abs(avgPos - AUTON_POS_TARGET) <= AUTON_POS_STOP_THRESHOLD)//pos is really close, stop
+			{
+				robotDrive.TankDrive(0.0,0.0);
+			}
+			else//HONEY, I THINK WE MISSED THE TURN
+			{
+				//TODO We can backup if we overshoot our target
+			}
+		}
+		else//encoders are not close... correct before continuing
+		{
+			//TODO this will take time to do.
+		}
 
 	}
-
 	void TeleopInit()
 	{
 		liftPos.Reset();
@@ -143,48 +277,73 @@ private:
 		leftArmEncoder.Reset();
 		lDriveEncoder.Reset();
 		rDriveEncoder.Reset();
-
 		data = 0 ;
 	}
 	void TeleopPeriodic()
 	{
+		arduinoReset.Set(1);
+		// GET CONTROLS STATE/ DO MOTIONS
 		lax = gamePad.GetRawAxis(0);
 		lay = gamePad.GetRawAxis(1);
 		rax = gamePad.GetRawAxis(2);
 		ray = gamePad.GetRawAxis(3);
-		if (!(0 || 0))
+		if ((lay>0)&&(!lifttop)&&!gamePad.GetRawButton(8))
 		{
-			if (lay>0) lay=lay*.4;
+			lay=lay*.4;
 			gearMotor.Set(-lay);
 		}
-		if (!(0 || 0)) leftArm.Set(-lax);
-		if (!(0 || 0)) rightArm.Set(rax);
-		if (!(0 || 0)) claw.Set(ray*.3);
+		if ((lay<0)&&(!liftbottom)&&!gamePad.GetRawButton(8))
+		{
+			gearMotor.Set(-lay);
+		}
+		if ((lax>0)&&(!LarmL)&&!gamePad.GetRawButton(7))
+		{
+			leftArm.Set(-lax);
+		}
+		if ((lax<0)&&(!LarmR)&&!gamePad.GetRawButton(7))
+		{
+			leftArm.Set(-lax);
+		}
+		if ((rax>0)&&(!RarmR)&&!gamePad.GetRawButton(7))
+		{
+			rightArm.Set(rax);
+		}
+		if ((rax<0)&&(!RarmL))
+		{
+			rightArm.Set(rax);
+		}
+		if ((ray<0)&&(!clawbottom))
+		{
+			ray = ray*.3;
+			claw.Set(ray);
+		}
 		// determine tilt
-			double xVal = accel->GetX();
-			double yVal = accel->GetY();
-			double zVal = accel->GetZ();
-			gmag = xVal*xVal+yVal*yVal+zVal*zVal;
-			if ((gmag<gmax*gmax)&&(gmag>gmin*gmin)) { //validate
-					gmag = pow(gmag,0.5);
-					tilt_theta = acos(zVal/gmag);
-					tilt_phi   = atan(yVal/xVal);
-			}
-			// end get tilt
-			// compute heading
-			Bmag = Bx*Bx+By*By+Bz*Bz;
-			if ((Bmag<Bmax*Bmax)&&(Bmag>Bmin*Bmin)){ //validate
-				Bmag = Bx;
-				Bx = cos(tilt_phi)*Bmag + sin(tilt_phi)*(cos(tilt_theta)*By+sin(tilt_theta)*Bz);
-				By = -sin(tilt_phi)*Bmag + cos(tilt_phi)*(cos(tilt_theta)*By+sin(tilt_theta)*Bz);
-				heading = atan(By/Bx);
-			}
-			// end compute heading
+		double xVal = accel->GetX();
+		double yVal = accel->GetY();
+		double zVal = accel->GetZ();
+		gmag = xVal*xVal+yVal*yVal+zVal*zVal;
+		if ((gmag<gmax*gmax)&&(gmag>gmin*gmin)) { //validate
+			gmag = pow(gmag,0.5);
+			tilt_theta = acos(zVal/gmag);
+			tilt_phi   = atan(yVal/xVal);
+		}
+		tilt_theta = acos(zVal/gmag);
+		tilt_phi   = atan(yVal/xVal);
+		// end get tilt
+		// compute heading
+		Bmag = Bx*Bx+By*By+Bz*Bz;
+		if ((Bmag<Bmax*Bmax)&&(Bmag>Bmin*Bmin)){ //validate
+			Bmag = Bx;
+			Bx = cos(tilt_phi)*Bmag + sin(tilt_phi)*(cos(tilt_theta)*By+sin(tilt_theta)*Bz);
+			By = -sin(tilt_phi)*Bmag + cos(tilt_phi)*(cos(tilt_theta)*By+sin(tilt_theta)*Bz);
+			heading = 180.0*atan(By/Bx)/pi;
+		}
+		// end compute heading
 		//button mappings : GAMEPAD
 		//X
 		if(gamePad.GetRawButton(1))
 		{
-			getHeading();
+			//getHeading();
 		}
 		//A
 		if(gamePad.GetRawButton(2))
@@ -194,7 +353,7 @@ private:
 		//B
 		if(gamePad.GetRawButton(3))
 		{
-			ArduinoRW(0xB);
+
 		}
 		//Y
 		if(gamePad.GetRawButton(4))
@@ -228,7 +387,8 @@ private:
 		//START
 		if(gamePad.GetRawButton(10))
 		{
-
+			// momentarily turn on the reset pin.
+			arduinoReset.Set(0);
 		}
 		//L stick
 		if(gamePad.GetRawButton(11))
@@ -240,34 +400,37 @@ private:
 		{
 
 		}
-		#ifdef MECHANUM
-			rx = x * sncs - y * sncs;
-			ry = x * sncs + y * sncs;
-			robotDrive.MecanumDrive_Cartesian(rx,ry,rateGyro.GetRate(),rateGyro.GetAngle());
-		#endif
-		#ifdef TANK
-			robotDrive.TankDrive(-leftStick.GetY(),-rightStick.GetY());
-		#endif
+#ifdef MECHANUM
+		rx = x * sncs - y * sncs;
+		ry = x * sncs + y * sncs;
+		robotDrive.MecanumDrive_Cartesian(rx,ry,rateGyro.GetRate(),rateGyro.GetAngle());
+#endif
+#ifdef TANK
+		robotDrive.TankDrive(-leftStick.GetY(),-rightStick.GetY());
+#endif
 
-			SmartDashboard::PutNumber("PDB Temp",(float)PDB.GetTemperature());
-			SmartDashboard::PutNumber("PDB Current Lift",PDB.GetCurrent(2));
-			SmartDashboard::PutNumber("PDB Current Left Arm",PDB.GetCurrent(3));
-			SmartDashboard::PutNumber("PDB Current Right Arm",PDB.GetCurrent(12));
+		SmartDashboard::PutNumber("PDB Temp",(float)PDB.GetTemperature());
+		SmartDashboard::PutNumber("PDB Current Lift",PDB.GetCurrent(2));
+		SmartDashboard::PutNumber("PDB Current Left Arm",PDB.GetCurrent(3));
+		SmartDashboard::PutNumber("PDB Current Right Arm",PDB.GetCurrent(12));
 
-			SmartDashboard::PutNumber("Lift Throttle",(int)25*lay);
-			SmartDashboard::PutNumber("Right Drive Throttle",(int)25*lay);
-			SmartDashboard::PutNumber("Left Drive Throttle",(int)25*lay);
-			SmartDashboard::PutNumber("Left Arm Throttle",(int)25*lay);
-			SmartDashboard::PutNumber("Right Arm Throttle",(int)25*lay);
+		SmartDashboard::PutNumber("Lift Throttle",(int)25*lay);
+		SmartDashboard::PutNumber("Right Drive Throttle",(int)25*lay);
+		SmartDashboard::PutNumber("Left Drive Throttle",(int)25*lay);
+		SmartDashboard::PutNumber("Left Arm Throttle",(int)25*lay);
+		SmartDashboard::PutNumber("Right Arm Throttle",(int)25*lay);
 
-			SmartDashboard::PutNumber("I2Cmsgs",counti2c);
+		SmartDashboard::PutNumber("I2Cmsgs",counti2c);
 
-			SmartDashboard::PutNumber("Lift Pos", liftPos.GetDistance());
-			SmartDashboard::PutNumber("Right Arm Pos", rightArmEncoder.GetRaw());
-			SmartDashboard::PutNumber("Left Arm Pos", leftArmEncoder.GetRaw());
-			SmartDashboard::PutNumber("Left Drive", lDriveEncoder.GetRaw());
-			SmartDashboard::PutNumber("Right Drive", rDriveEncoder.GetRaw());
+		SmartDashboard::PutNumber("Lift Pos", liftPos.GetDistance());
+		SmartDashboard::PutNumber("Right Arm Pos", rightArmEncoder.GetRaw());
+		SmartDashboard::PutNumber("Left Arm Pos", leftArmEncoder.GetRaw());
+		SmartDashboard::PutNumber("Left Drive", lDriveEncoder.GetRaw());
+		SmartDashboard::PutNumber("Right Drive", rDriveEncoder.GetRaw());
 
+		SmartDashboard::PutNumber("tilt theta", 180.0*tilt_theta/pi);
+		SmartDashboard::PutNumber("tilt phi", 180.0*tilt_phi/pi);
+		SmartDashboard::PutNumber("accel. magnitude", gmag);
 	}
 
 	void DisabledInit()
@@ -279,7 +442,6 @@ private:
 	{
 
 	}
-
 	void TestPeriodic()
 	{
 		lw->Run();
@@ -301,33 +463,114 @@ private:
 			counti2c++;
 			printf("Arduino: %X\n",*buffer[1]);
 		}
+		// parse switch state
+		switches = (int)(buffer[1]);
+		lifttop = switches & 1;
+		liftbottom = switches & 2;
+		RarmR = switches & 4;
+		RarmL = switches & 8;
+		LarmR = switches & 16;
+		LarmR = switches & 32;
+		clawbottom = switches & 64;
 	}
 	void getHeading()
 	{
 		I2C Wire(I2C::kOnboard, 0x1E);
-
 		Wire.Write(0x02,0x00);
 		//Wire.Write(0xA,payload);
-
-		try
-		{
+		try{
 			uint8_t *bufferz[7];
-			if(!Wire.Read(0x03,6, *bufferz))
-			{
+			if(!Wire.Read(0x03,6, *bufferz)){
 				counti2c++;
-				for(int q=0;q <=6;q++)
-				{
-				printf("byte:%i data:%X\n",q,*bufferz[q]);
+				for(int q=0;q <=6;q++){
+					printf("byte:%i data:%X\n",q,*bufferz[q]);
 				}
 			}
 		}
-		catch(int e)
-		{
+		catch(int e){
 			printf("HI %i\n",e);
 		}
 	}
-
-
+	void Auton() { //The Auton Code, structured right now as drive-grab-drive
+		//first drive forward
+		while(behavior_auton<3) {
+			time_auton = timed.Get();
+#ifdef MECHANUM   //drive forward
+			rx = x * sncs - y * sncs;
+			ry = x * sncs + y * sncs;
+			robotDrive.MecanumDrive_Cartesian(rx,ry,rateGyro.GetRate(),rateGyro.GetAngle());
+#endif
+#ifdef TANK
+			robotDrive.TankDrive(speed_x,speed_y); //trim these values for driving straight
+#endif
+		}
+		if(behavior_auton==3){// pick up package
+			if((leftArmPosn<LarmPreset)&&(!completel)){leftArm.Set(200);
+			}
+			else{
+				completel=completel||1;
+			}
+			if((rightArmPosn<RarmPreset)&&(!completer)){rightArm.Set(200);
+			}
+			else{
+				completer=completer||1;
+			}
+			if((behavior_auton==3)&&(completel&&completer)&&(liftPosn<lift_this_high)){
+				gearMotor.Set(200); // raise up
+			}
+			else{
+				if((behavior_auton==3)&&(completel&&completer)){
+					behavior_auton=7; // drive forward since completed lift
+				}
+			}
+		}
+		if(behavior_auton==7) {
+			time_auton = timed.Get();
+#ifdef MECHANUM   //drive forward
+			rx = x * sncs - y * sncs;
+			ry = x * sncs + y * sncs;
+			robotDrive.MecanumDrive_Cartesian(rx,ry,rateGyro.GetRate(),rateGyro.GetAngle());
+#endif
+#ifdef TANK
+			robotDrive.TankDrive(speed_x,speed_y); //trim these values for driving straight
+#endif
+		}
+		if(behavior_auton==15){ // drop package and set up last drive.
+			if(liftPosn>drop_this_low){
+				gearMotor.Set(-200); // lower gantry arms
+			}
+			if((liftPosn<drop_this_low)&&(!(completel&&completer))){
+				if((leftArmPosn<LarmPreset)&&(!completel)){leftArm.Set(-200);
+				}
+				else{
+					completel=completel||1;
+				}
+				if((rightArmPosn<RarmPreset)&&(!completer)){rightArm.Set(-200);
+				}
+				else{
+					completer=completer||1;
+				}
+			}
+			if((liftPosn<drop_this_low)&&(completel&&completer)){
+				behavior_auton=31; // all clear to drive away!
+			}
+		}
+		if(behavior_auton==31){
+			time_auton = timed.Get();
+#ifdef MECHANUM   //drive backwards a bit
+			rx = x * sncs - y * sncs;
+			ry = x * sncs + y * sncs;
+			robotDrive.MecanumDrive_Cartesian(rx,ry,rateGyro.GetRate(),rateGyro.GetAngle());
+#endif
+#ifdef TANK
+			robotDrive.TankDrive(-speed_x,-speed_y); //trim these values for driving straight
+#endif
+		}
+		if (time_auton == (1.0 + delay)) { // some timeout delay structure here. Not yet implemented...to make the auton failure resistant
+		}
+		else if (time_auton > (1.0 + 2.0*delay)) {
+		}
+	}
 };
 START_ROBOT_CLASS(Robot);
 
@@ -362,12 +605,12 @@ digital I/O                               |
 8  Lift capacitive-A                      |
 9  Lift capacitive-B                      |
                                           |
-Expansion Digital I/O (these are not )    |
+Expansion Digital I/O                     |
 10 Claw closed limit IN                   |
 11 R. Arm capacitive sync IN              |
 12 L. Arm capacitive sync IN              |
 13                                        |
-14                                        |
+14 Arduino reset line     OUT             |
 15                                        |
 16 rPi sync pulse         OUT             |
 17 rPi power reset line   OUT             |
@@ -393,4 +636,4 @@ Arduino (switches)                                  |
                                           |
 built in accelerometer: detecting tilt    |
 ------------------------------------------/
-*/
+ */
